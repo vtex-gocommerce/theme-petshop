be0150bdfbb28d0222efd50a795366cd2b173654
Renato S Moreno <renatomorenocz@gmail.com>
2018-11-20 14:21:05 -0200

    Improved css structure

--- react/FashionTheme.js
+++ react/FashionTheme.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react'
 import Cookies from 'js-cookie'
 
 import './global.css'
-import './header.global.css'
-import './list-products.global.css'
-import './footer.global.css'
-import './product.global.css'
-import './minicart.sidebar.global.css'
-import './minicart.popup.global.css'
 
 const VTEX_ENV = 'beta'
 
