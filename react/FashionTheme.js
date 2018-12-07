5a502f9c531208c05f5fecf1ab2da5916bc981f0
Gustavo Rosolem <guspace92@hotmail.com>
2018-12-07 11:33:13 -0200

    Remove setCookie

--- react/FashionTheme.js
+++ react/FashionTheme.js
@@ -1,16 +1,1 @@
-import React, { Component } from 'react'
-import Cookies from 'js-cookie'
-
 import './global.css'
-
-const VTEX_ENV = 'beta'
-
-export default class FashionTheme extends Component {
-  componentDidMount() {
-    Cookies.set('vtex-commerce-env', VTEX_ENV)
-  }
-
-  render() {
-    return null
-  }
-}
